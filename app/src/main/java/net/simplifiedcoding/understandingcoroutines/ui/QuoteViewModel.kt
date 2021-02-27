10c9301d79e5222973bdefc7c8d4f2f3dacfff15
BorisKunda <bskunda1992@gmail.com>
2021-02-27 17:56:10 +0200

    syntax improved

--- app/src/main/java/net/simplifiedcoding/understandingcoroutines/ui/QuoteViewModel.kt
+++ app/src/main/java/net/simplifiedcoding/understandingcoroutines/ui/QuoteViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import net.simplifiedcoding.understandingcoroutines.data.models.Movie
 import net.simplifiedcoding.understandingcoroutines.data.models.Quote
 import net.simplifiedcoding.understandingcoroutines.data.network.MyApi
@@ -19,27 +21,39 @@ class QuoteViewModel : ViewModel() {
         viewModelScope.launch {
             quotes as MutableLiveData
             for (i in 1..5) {
-                val quotes1: Deferred<List<Quote>?> = async { getQuotes() }
-                val quotes2: Deferred<List<Quote>?> = async { getQuotes() }
-                val quotes3: Deferred<List<Quote>?> = async { getQuotes() }
+                android.util.Log.i(TAG, "Start $i")
+                //async
+                // val quotes1: Deferred<List<Quote>?> = async { getQuotes("1") }
+                // val quotes2: Deferred<List<Quote>?> = async { getQuotes("2") }
+                // val quotes3: Deferred<List<Quote>?> = async { getQuotes("3") }
 
-                val movies1: Deferred<List<Movie>?> = async { getMovies() }
-                val movies2: Deferred<List<Movie>?> = async { getMovies() }
-                val movies3: Deferred<List<Movie>?> = async { getMovies() }
+                // val movies1: Deferred<List<Movie>?> = async { getMovies("1") }
+                // val movies2: Deferred<List<Movie>?> = async { getMovies("2") }
+                // val movies3: Deferred<List<Movie>?> = async { getMovies("3") }
+                //--regular--
+                val quotes1: List<Quote>? = getQuotes("1")
+                val quotes2: List<Quote>? = getQuotes("2")
+                val quotes3: List<Quote>? = getQuotes("3")
+
+                val movies1: List<Movie>? = getMovies("1")
+                val movies2: List<Movie>? = getMovies("2")
+                val movies3: List<Movie>? = getMovies("3")
+
+                android.util.Log.i(TAG, "Finish $i")
             }
         }
     }
 
-    private suspend fun getQuotes(): List<Quote>? {
+    private suspend fun getQuotes(index: String): List<Quote>? {
         return withContext(Dispatchers.IO) {
-            android.util.Log.i(TAG, "Getting Quotes")
+            android.util.Log.i(TAG, "Getting Quotes---$index")
             MyApi().getQuotes().body()?.quotes
         }
     }
 
-    private suspend fun getMovies(): List<Movie>? {
+    private suspend fun getMovies(index: String): List<Movie>? {
         return withContext(Dispatchers.IO) {
-            android.util.Log.e(TAG, "Getting Movies")
+            android.util.Log.i(TAG, "Getting Movies---$index")
             MyApi().getMovies().body()
         }
     }
